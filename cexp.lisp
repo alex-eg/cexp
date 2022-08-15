f5e16865e7028a4a09c710f59a38994012b7b22f
alex-eg <alexey.e.egorov@gmail.com>
2022-08-15 18:18:27 +0300

    Improve var and type producing

--- cexp.lisp
+++ cexp.lisp
@@ -3,28 +3,66 @@
 
 (in-package :cexp)
 
-(defparameter syms '((include "<stdio.h>")
+(defvar *type-keywords* '(|char| |int| |long| |short|
+                          |float| |double|
+                          |_Bool|
+                          |signed| |unsigned|))
 
-                     (fn main () -> int
-                      (int i 12)
-                      (int j 34)
-                      (lambda foo [i &j] (int )
-                              (set i 56)
-                              (set j 78)
-                              (printf "Inside foo: %d %d\n" i j))
-                      (printf "Outside foo: %d %d\n" i j)
-;                      (foo)
-                      (return 0))))
+(defvar *qualifier-keywords* '(|static| |auto| |restrict|
+                               |_Atomic| |_Thread_local|
+                               |const| |volatile|))
 
-(defparameter *lambdas* (list))
+(defun produce-typedef (form)
+  (dolist (var (cdr form))
+    (progn)))
 
-(defun flatten (&rest strings)
-  (reduce (lambda (acc str)
-            (concatenate 'string acc
-                         (if (listp str)
-                           (apply #'flatten str)
-                           str)))
-          strings :initial-value ""))
+(defun sort-type-declaration (form)
+  (sort form (lambda (a b)
+               (declare (ignore a))
+               (listp b))))
+
+(defun produce-type-declaration (form)
+  ;; TODO: checks! no more than 1 ptr per level,
+  ;; no more than 1 type in the whole form, check for
+  ;; incompatible type kws and qualificators
+  (labels ((parse (form acc)
+             (if (null form) acc
+               (if (listp (car form))
+                 (progn
+                   (assert (eq 'ptr (caar form)))
+                   (cons (nreverse (parse (cdar form) (list)))
+                         (cons "*" acc)))
+                 (parse (cdr form)
+                        (cons (to-str (car form)) acc))))))
+    (parse (sort-type-declaration form) (list))))
+
+(defun produce-variable-declaration (form)
+  (assert (eq 'var (car form)))
+  (let ((form (cdr form)))
+    (str+ (flatten (produce-type-declaration (cdr form)) :delimiter " ")
+          " " (to-str (car form)) ";")))
+
+(defun str+ (&rest strs)
+  (apply #'concatenate 'string strs))
+
+(defun flatten (strings &key (delimiter ""))
+  (let ((args
+          (nreverse
+           (reduce (lambda (acc arg)
+                     (if (eq (car acc) :delimiter)
+                       (cdr acc)
+                       (cons arg acc)))
+                   strings :initial-value (list)))))
+    (labels ((process (acc strs delimiter)
+               (reduce (lambda (acc str)
+                         (concatenate 'string acc
+                                      (if (listp str)
+                                        (process "" str delimiter)
+                                        (str+ str delimiter))))
+                       strs :initial-value acc)))
+      (let ((res (process "" args delimiter)))
+        (subseq res 0 (- (length res)
+                         (length delimiter)))))))
 
 (defun parse-arglist (form)
   (nreverse
@@ -34,7 +72,11 @@
                          ((numberp token)
                           (to-str-num token))
                          ((stringp token)
-                          (format nil "~s" token)))
+                          (format nil "\"~a\"" (map 'string (lambda (char)
+                                                          (if (char= char #\~)
+                                                            #\\
+                                                            char))
+                                                token))))
                    acc))
            form :initial-value (list))))
 
@@ -47,27 +89,16 @@
 (defun to-str-num (num)
   (format nil "~S" num))
 
-(defun parse-args (form)
-  "(void)")
-
-(defun parse-lambda (form)
-  "")
-
 (defun parse-statement (form)
   (case (car form)
-    (int
-       (flatten "int " (to-str (cadr form))
-                (if (> (length form) 2)
-                  (flatten " = " (to-str-num (caddr form)) ";")
-                  ";")))
-    (lambda (parse-lambda (cdr form)))
+    (var
+       (produce-variable-declaration form))
     (otherwise (parse-funcall form))))
 
 (defun parse-fn (form)
   (let ((fn-strings (list)))
     (push (sb-unicode:lowercase (string (cadddr form))) fn-strings)
     (push (sb-unicode:lowercase (string (car form))) fn-strings)
-    (push (parse-args (cadr form)) fn-strings)
     (push "{" fn-strings)
     (dolist (line (nthcdr 4 form))
       (push (parse-statement line) fn-strings))
