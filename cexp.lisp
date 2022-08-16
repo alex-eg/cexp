402e3b75dfd9b8bf297763bb068bd107d9a4e3da
alex-eg <alexey.e.egorov@gmail.com>
2022-08-16 18:42:16 +0300

    Improve type and variable producing further

--- cexp.lisp
+++ cexp.lisp
@@ -16,10 +16,9 @@
   (dolist (var (cdr form))
     (progn)))
 
-(defun sort-type-declaration (form)
-  (sort form (lambda (a b)
-               (declare (ignore a))
-               (listp b))))
+(defun produce-ptr-declaration (form)
+  (assert (eq '|ptr| (car form)))
+  (list (produce-type-declaration (cdr form)) "*"))
 
 (defun produce-type-declaration (form)
   ;; TODO: checks! no more than 1 ptr per level,
@@ -28,19 +27,35 @@
   (labels ((parse (form acc)
              (if (null form) acc
                (if (listp (car form))
-                 (progn
-                   (assert (eq 'ptr (caar form)))
-                   (cons (nreverse (parse (cdar form) (list)))
-                         (cons "*" acc)))
+                 (parse (cdr form)
+                        (cons (produce-ptr-declaration (car form)) acc))
                  (parse (cdr form)
                         (cons (to-str (car form)) acc))))))
-    (parse (sort-type-declaration form) (list))))
+    (parse form (list))))
+
+(defun produce-variable-name (name)
+  (map 'string
+       (lambda (char)
+         (if (char= #\- char)
+           #\_
+           char))
+       (to-str name)))
+
+(defun produce-expression (form)
+  (labels ((process (form acc)
+             nil))))
 
 (defun produce-variable-declaration (form)
-  (assert (eq 'var (car form)))
-  (let ((form (cdr form)))
-    (str+ (flatten (produce-type-declaration (cdr form)) :delimiter " ")
-          " " (to-str (car form)) ";")))
+  ;; (var name (<type>) [<value>])
+  (assert (eq '|var| (car form)))
+  (let ((name (second form))
+        (type (third form)))
+    (append
+     (produce-type-declaration type)
+     (list (produce-variable-name name))
+     (when (= 4 (length form))
+       (produce-expression (fourth form)))
+     (list ";"))))
 
 (defun str+ (&rest strs)
   (apply #'concatenate 'string strs))
@@ -64,6 +79,14 @@
         (subseq res 0 (- (length res)
                          (length delimiter)))))))
 
+(defun process-string (string)
+  (format nil "\"~a\""
+          (map 'string (lambda (char)
+                         (if (char= char #\~)
+                           #\\
+                           char))
+               string)))
+
 (defun parse-arglist (form)
   (nreverse
    (reduce (lambda (acc token)
@@ -72,11 +95,7 @@
                          ((numberp token)
                           (to-str-num token))
                          ((stringp token)
-                          (format nil "\"~a\"" (map 'string (lambda (char)
-                                                          (if (char= char #\~)
-                                                            #\\
-                                                            char))
-                                                token))))
+                          (process-string token)))
                    acc))
            form :initial-value (list))))
 
@@ -84,21 +103,21 @@
   (format nil "~a(~{~a~^, ~});" (to-str (car form)) (parse-arglist (cdr form))))
 
 (defun to-str (sym)
-  (sb-unicode:lowercase (string sym)))
+  (string sym))
 
 (defun to-str-num (num)
   (format nil "~S" num))
 
 (defun parse-statement (form)
   (case (car form)
-    (var
-       (produce-variable-declaration form))
+    (|var|
+       (flatten (produce-variable-declaration form) :delimiter " "))
     (otherwise (parse-funcall form))))
 
 (defun parse-fn (form)
   (let ((fn-strings (list)))
-    (push (sb-unicode:lowercase (string (cadddr form))) fn-strings)
-    (push (sb-unicode:lowercase (string (car form))) fn-strings)
+    (push (string (cadddr form)) fn-strings)
+    (push (string (car form)) fn-strings)
     (push "{" fn-strings)
     (dolist (line (nthcdr 4 form))
       (push (parse-statement line) fn-strings))
@@ -109,9 +128,9 @@
   (let ((program-strings (list)))
     (dolist (form program)
       (case (car form)
-        (include (push "#include" program-strings)
+        (|include| (push "#include" program-strings)
            (push (format nil "~s" (cadr form)) program-strings)
            (push #\Newline program-strings))
-        (fn (setf program-strings (nconc (parse-fn (cdr form)) program-strings)))
+        (|fn| (setf program-strings (nconc (parse-fn (cdr form)) program-strings)))
         (otherwise (format t "Unknown key ~S (package ~S)~%" (car form) (symbol-package (car form))))))
     (format nil "~{~a~^ ~}" (nreverse program-strings))))
