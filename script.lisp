2ef2ac365b1769795042d4888ecafcda36411f2f
alex-eg <alexey.e.egorov@gmail.com>
2022-08-16 18:42:45 +0300

    Enable script to read file preserving case

--- script.lisp
+++ script.lisp
@@ -1,5 +1,10 @@
 (load "~/.sbclrc")
 (ql:quickload :cexp :silent t)
 (in-package :cexp)
-(let ((forms (uiop:read-file-forms (cadr sb-ext:*posix-argv*))))
+(let ((forms
+        (eval-when (:compile-toplevel :load-toplevel :execute)
+          (setf (readtable-case *readtable*) :preserve)
+          (PROG1
+              (UIOP:READ-FILE-FORMS (CADR SB-EXT:*POSIX-ARGV*))
+            (SETF (READTABLE-CASE *READTABLE*) :UPCASE)))))
   (format t "~a" (cexp::process forms)))
